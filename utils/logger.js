6f6647c84a9b456fef7e1b1eaf22a39ec6e5e0c3
LeSynz <bigman@synz.xyz>
2025-11-26 18:46:59 +0000

    New logger?

--- utils/logger.js
+++ utils/logger.js
@@ -1,32 +1,49 @@
-const colors = {
-	reset: '\x1b[0m',
-	bright: '\x1b[1m',
-	dim: '\x1b[2m',
-	red: '\x1b[31m',
-	green: '\x1b[32m',
-	yellow: '\x1b[33m',
-	blue: '\x1b[34m',
-	magenta: '\x1b[35m',
-	cyan: '\x1b[36m',
-	white: '\x1b[37m',
-};
+const util = require("util");
 
-function formatTime() {
+// testing bc pterodactyl is a bitch <3
+const supportsColor = process.stdout && process.stdout.isTTY;
+
+const colors = supportsColor
+	? {
+		reset: "\x1b[0m",
+		red: "\x1b[31m",
+		green: "\x1b[32m",
+		yellow: "\x1b[33m",
+		blue: "\x1b[34m",
+		magenta: "\x1b[35m",
+		cyan: "\x1b[36m",
+		white: "\x1b[37m",
+	  }
+	: {
+		reset: "",
+		red: "",
+		green: "",
+		yellow: "",
+		blue: "",
+		magenta: "",
+		cyan: "",
+		white: "",
+	  };
+
+function timestamp() {
 	return new Date().toLocaleString();
 }
 
 function log(level, color, ...args) {
-	const timestamp = formatTime();
-	const message = `${color}[${timestamp}] [${level}]${colors.reset} ${args.map(arg => 
-		typeof arg === 'object' ? JSON.stringify(arg) : String(arg)
-	).join(' ')}\n`;
-	process.stdout.write(message);
+	const prefix = `[${timestamp()}] [${level}]`;
+
+	const formatted =
+		args.length === 1 && typeof args[0] === "object"
+			? util.inspect(args[0], { depth: 5, colors: false })
+			: args.map(x => String(x)).join(" ");
+
+	console.log(`${color}${prefix}${colors.reset} ${formatted}`);
 }
 
 module.exports = {
-	info: (...args) => log('INFO', colors.blue, ...args),
-	warn: (...args) => log('WARN', colors.yellow, ...args),
-	error: (...args) => log('ERROR', colors.red, ...args),
-	success: (...args) => log('SUCCESS', colors.green, ...args),
-	debug: (...args) => log('DEBUG', colors.magenta, ...args),
+	info: (...a) => log("INFO", colors.blue, ...a),
+	warn: (...a) => log("WARN", colors.yellow, ...a),
+	error: (...a) => log("ERROR", colors.red, ...a),
+	success: (...a) => log("SUCCESS", colors.green, ...a),
+	debug: (...a) => log("DEBUG", colors.magenta, ...a),
 };
